33bf2994650a7f24fe5d601fdc3fbac779d79c59
amirali monjar <amirali.mnj@gmail.com>
2019-05-02 11:32:31 +0430

    Contact us View updated.

---
+++ resources/views/contact_us.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
+<h2>About Us</h2>
+
+<table style="width:100%">
+  <tr>
+    <th>Pages</th>
+  </tr>
+  <tr>
+    <td><a href="home">Home Page</a></td>
+  </tr>
+  <tr>
+    <td><a href="about_us">About Us</a></td>
+  </tr>
+ 
+</table>
+
+</body>
+</html>
+
